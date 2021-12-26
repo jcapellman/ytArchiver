9d7c7448ad39396a60ff100aa3e35283fb75d474
Jarred Capellman <jcapellman@hotmail.com>
2021-12-26 14:14:47 -0500

    Added: MainViewModel

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+using ytArchiver.Objects;
+
+namespace ytArchiver.ViewModel
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<YTVideoItem> _videoItems;
+
+        public ObservableCollection<YTVideoItem> VideoItems
+        {
+            get => _videoItems;
+
+            set
+            {
+                _videoItems = value;
+
+                OnPropertyChanged();
+            }
+        }
+
+        public MainViewModel()
+        {
+            VideoItems = new ObservableCollection<YTVideoItem>();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
