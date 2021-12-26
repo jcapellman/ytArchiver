7f9775df5ab5d773a96cd6f4bf3929115ef2b79e
Jarred Capellman <jcapellman@hotmail.com>
2021-12-26 14:14:34 -0500

    Added: YTVideoItem

---
+++ Objects/YTVideoItem.cs
@@ -0,0 +1,15 @@
+﻿using ytArchiver.Enums;
+
+namespace ytArchiver.Objects
+{
+    public class YTVideoItem
+    {
+        public string Title { get; set; }
+
+        public VideoStatus Status { get; set; }
+
+        public string Resolution { get; set; }
+
+        public string FileName { get; set; }
+    }
+}
