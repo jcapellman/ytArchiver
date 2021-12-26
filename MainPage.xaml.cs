7096f700e032b89152c6f347c36c720cc717be55
Jarred Capellman <jcapellman@hotmail.com>
2021-12-26 14:37:13 -0500

    Added: Initial work on the UI

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -6,11 +6,23 @@ namespace ytArchiver
 {
     public sealed partial class MainPage : Page
     {
+        private MainViewModel ViewModel => (MainViewModel)DataContext;
+
         public MainPage()
         {
             this.InitializeComponent();
 
             DataContext = new MainViewModel();
+        }
+
+        private void btnAddToQueue_Click(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            var status = ViewModel.AddDownloadToQueue();
+
+            if (!status)
+            {
+                // TODO: Message Box error
+            }
         }
     }
 }
