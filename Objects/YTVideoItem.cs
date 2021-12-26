79f09509edf67d0e507aedffd277f71bd76f7a98
Jarred Capellman <jcapellman@hotmail.com>
2021-12-26 14:28:50 -0500

    Added: YT Download Logic

--- Objects/YTVideoItem.cs
+++ Objects/YTVideoItem.cs
@@ -1,4 +1,8 @@
-﻿using ytArchiver.Enums;
+﻿using System.IO;
+
+using ytArchiver.Enums;
+
+using VideoLibrary;
 
 namespace ytArchiver.Objects
 {
@@ -11,5 +15,40 @@ namespace ytArchiver.Objects
         public string Resolution { get; set; }
 
         public string FileName { get; set; }
+
+        private YouTubeInfo ytInfo { get; set; }
+
+        public string DownloadPath { get; set; }
+
+        public YTVideoItem() { }
+
+        public YTVideoItem(string youTubeURL, string downloadPath)
+        {
+            var videoInfos = YouTube.Default.GetAllVideos(youTubeURL);
+
+            if (videoInfos == null)
+            {
+                Status = VideoStatus.ERROR;
+            }
+
+            ytInfo = videoInfos.FirstOrDefault(a => a.Resolution == 1080);
+
+            DownloadPath = Path.Combine(downloadPath, ytInfo.FullName);
+
+            Title = ytInfo.Title;
+            Resolution = ytInfo.Resolution;
+            FileName = ytInfo.FullName;
+
+            Status = VideoStatus.QUEUED;
+        }
+
+        public void Download()
+        {
+            Status = VideoStatus.DOWNLOADING;
+
+            File.WriteAllBytes(DownloadPath, ytInfo.GetBytes());
+
+            Status = VideoStatus.DOWNLOADED;
+        }
     }
 }
